40b2db4e7f1c99e954cc0e1bf74733afcda687a1
nokranger <anusorn.thavornporn@gmail.com>
2021-07-14 15:24:44 +0700

    props

---
+++ src/views/dash.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <blog-post v-for="(testss, index) in test" :key="index" :titles="testss.title"></blog-post>
+    <blog-post titles="Props"></blog-post>
+    <blog-post titles="test"></blog-post>
+    <blog-post titles="Prosp" bgColor="black" :height="200" color="white" url="https://www.youtube.com/watch?v=W2j7CjQI2y2"></blog-post>
+  </div>
+</template>
+<script>
+import propss from '../views/props'
+export default {
+  components: {
+    'blog-post': propss
+  },
+  data () {
+    return {
+      test: [
+        { id: '1', title: 'fsafasfasfa' },
+        { id: '2', title: 'fasfasfasasf33rf3r3' },
+        { id: '3', title: 'xcbcxbxbxcbxb' }
+      ],
+      cssProps: {
+        'background-color': 'white',
+        height: '200px',
+        color: 'black'
+      }
+    }
+  },
+  computed: {
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/views/props.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h3 :style="btnStyles">{{titles}}</h3>
+    <button>AAAA</button>
+    {{test}}
+    <video width="400" controls>
+      <source :src="url" type="video/mp4">
+      <source :src="url" type="video/ogg">
+      not support
+    </video>
+    <!-- <blog-post title="test eiei"></blog-post> -->
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    titles: {
+      type: String,
+      required: true,
+      default: 'This is default'
+    },
+    bgColor: {
+      type: String,
+      default: '#0099CC'
+    },
+    height: {
+      type: Number,
+      default: 100
+    },
+    color: {
+      type: String,
+      default: 'red'
+    },
+    url: {
+      type: String,
+      default: 'https://www.youtube.com/watch?v=W2j7CjQI2y4'
+    }
+  },
+  mounted () {
+  },
+  computed: {
+    btnStyles () {
+      return {
+        'background-color': this.bgColor,
+        height: `${this.height}px`,
+        color: this.color
+      }
+    },
+    test () {
+      return console.log(this.url)
+    }
+  }
+}
+</script>
+<style>
+</style>
